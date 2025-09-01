03cc68191cecdfafb98615c620cf1f8d5bb37947
HassanMunene <awanzihassan@gmail.com>
2025-09-01 21:18:26 +0300

    Authentication controller to register users

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,72 @@
+import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import prisma from '../prisma/index';
+
+// Here we will just register a new farmer Admin is already pre-seeded
+export const registerUser = asyncHandler(async (req, res) => {
+    const { email, password, fullName, location, contactInfo } = req.body;
+
+    // 1. Validation
+    if (!email || !password || !fullName) {
+        res.status(400);
+        throw new Error('Please include email, password, and full name');
+    }
+
+    // 2. Check if user exists
+    const userExists = await prisma.user.findUnique({ where: { email } });
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    // 3. Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // 4. Create User and Profile in a transaction (ATOMIC)
+    const newUser = await prisma.$transaction(async (tx) => {
+        const user = await tx.user.create({
+            data: {
+                email,
+                password: hashedPassword,
+                role: 'FARMER', // This is the default role.
+            },
+        });
+
+        const profile = await tx.profile.create({
+            data: {
+                fullName,
+                location,
+                contactInfo,
+                user: { connect: { id: user.id } },
+            },
+        });
+
+        // Return user data without password and with profile
+        return {
+            id: user.id,
+            email: user.email,
+            role: user.role,
+            createdAt: user.createdAt,
+            profile: profile,
+        };
+    });
+
+    // 5. Generate Token and respond
+    generateToken(res, newUser.id);
+    res.status(201).json(newUser);
+});
+
+// Generate JWT and set it in an HTTP-Only cookie
+const generateToken = (res, userId) => {
+    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+
+    // Set JWT as HTTP-Only cookie
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
+};
