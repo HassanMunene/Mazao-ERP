a43eda8b0b0a1fbde4f0bd71c6b391b421ab7d57
HassanMunene <awanzihassan@gmail.com>
2025-09-01 19:48:58 +0300

    Error Middleware setup

---
+++ backend/src/middleware/errorMiddleware.js
@@ -0,0 +1,20 @@
+const errorHandler = (err, req, res, next) => {
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message;
+
+    // Prisma specific error handling
+    if (err.code === 'P2025') {
+        statusCode = 404;
+        message = 'Resource not found';
+    } else if (err.code === 'P2002') {
+        statusCode = 400;
+        message = 'A unique field constraint was violated';
+    }
+
+    res.status(statusCode).json({
+        message,
+        stack: process.env.NODE_ENV === 'production' ? 'ErrorStack' : err.stack,
+    });
+};
+
+export { errorHandler };
